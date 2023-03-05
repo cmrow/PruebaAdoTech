e9f1ce08e670904fb15b0cf7ec436a3480e0e94b
Carlos Mario <cmrodriguez261@misena.edu.co>
2023-03-05 13:38:07 -0500

    Se crea contexto

---
+++ PruebaAdoTech/Datos/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaAdoTech.Modelos;
+
+namespace PruebaAdoTech.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Cliente> Cliente { get; set; }
+    }
+}
