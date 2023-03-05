e4e3ccfc3f305b043d3117e09e30f6625e44d706
Carlos Mario <cmrodriguez261@misena.edu.co>
2023-03-05 13:38:36 -0500

    Se crea modelo Cliente

---
+++ PruebaAdoTech/Modelos/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PruebaAdoTech.Modelos
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Número de identificación es un campo requerido")]
+        public string NumeroIdentificacion { get; set; }
+
+        [Required(ErrorMessage = "Nombres es un campo requerido")]
+        public string Nombres { get; set; }
+
+        [Required(ErrorMessage = "Apellidos es un campo requerido")]
+        public string Apellidos { get; set; }
+
+        public string Genero { get; set; }
+    }
+}
